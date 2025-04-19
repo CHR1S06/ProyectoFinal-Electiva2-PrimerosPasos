e4029a0ab39d72f37581908d38cdf18123d2f794
CHR1S06 <cenmanuelnavarro@gmail.com>
2025-04-18 22:55:56 -0400

    Change of vitest to jest

--- index.test.js
+++ index.test.js
@@ -1,12 +1,11 @@
 // index.test.js
-import { describe, it, expect } from 'vitest';
-import { JSDOM } from 'jsdom';
 import fs from 'fs';
+import { JSDOM } from 'jsdom';
 
 const html = fs.readFileSync('index.html', 'utf-8');
 
 describe('HTML content', () => {
-  it('contiene Hola Mundo en h1', () => {
+  test('contiene Hola Mundo en h1', () => {
     const dom = new JSDOM(html);
     const h1 = dom.window.document.querySelector('h1');
     expect(h1.textContent).toBe('Hola Mundo');
