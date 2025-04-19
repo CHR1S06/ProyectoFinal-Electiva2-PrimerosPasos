65de597e19df3e413f616bd8e5eb86b6201ac255
CHR1S06 <cenmanuelnavarro@gmail.com>
2025-04-19 12:00:35 -0400

    Test Push

--- index.test.js
+++ index.test.js
@@ -1,11 +1,10 @@
-// index.test.js
-import fs from 'fs';
-import { JSDOM } from 'jsdom';
+const { JSDOM } = require('jsdom');
+const fs = require('fs');
 
 const html = fs.readFileSync('index.html', 'utf-8');
 
 describe('HTML content', () => {
-  test('contiene Hola Mundo en h1', () => {
+  it('contiene Hola Mundo en h1', () => {
     const dom = new JSDOM(html);
     const h1 = dom.window.document.querySelector('h1');
     expect(h1.textContent).toBe('Hola Mundo');
